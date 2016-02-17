0019c108858905d941a209c4109801a9c407060e
Dmitriy Dubson <d.dubson@gmail.com>
2016-02-16 23:21:29 -0500

    Added Balanced BST algorithm

---
+++ src/main/java/com/ddubson/datastructures/BalancedBST.java
@@ -0,0 +1,26 @@
+package com.ddubson.datastructures;
+
+/**
+ * Created by ddubson on 2/16/16.
+ */
+public class BalancedBST {
+	public static void main(String... args) {
+		int[] sortedArray = { 1, 2, 4, 5, 6, 7, 8 };
+
+		TreeNode n = createBST(sortedArray, 0, sortedArray.length - 1);
+		System.out.println(n.toString());
+	}
+
+	private static TreeNode createBST(int[] array, int start, int end) {
+		if (start > end) {
+			return null;
+		}
+
+		int mid = (start + end) / 2;
+		TreeNode root = new TreeNode(array[mid]);
+		root.setLeft(createBST(array, start, mid - 1));
+		root.setRight(createBST(array, mid + 1, end));
+
+		return root;
+	}
+}
---
+++ src/main/java/com/ddubson/datastructures/TreeNode.java
@@ -0,0 +1,38 @@
+package com.ddubson.datastructures;
+
+/**
+ * Created by ddubson on 2/16/16.
+ */
+public class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int value) {
+		this.value = value;
+	}
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+
+	public String toString() {
+		return "{ value: " + value + ", \nleft: " + (left != null ?
+				left.toString() :
+				"end") + ", \nright: " + (right != null ?
+				right.toString() :
+				"end") + " }\n";
+	}
+}
