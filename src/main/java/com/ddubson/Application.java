7582444176a00773a941aee77849564dcba56c9b
Dmitriy Dubson <d.dubson@manahealth.com>
2016-03-07 17:06:54 -0500

    Added gradle wrapper

---
+++ src/main/java/com/ddubson/Application.java
@@ -0,0 +1,10 @@
+package com.ddubson;
+
+/**
+ * Created by d.dubson on 3/7/2016.
+ */
+public class Application {
+	public static void main(String... args) {
+		System.out.print("Application started.");
+	}
+}
