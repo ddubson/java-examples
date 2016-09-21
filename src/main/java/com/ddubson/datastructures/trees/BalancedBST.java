01d2945c07db0f7d796c126cfc9a79f19064e44d
Dmitriy Dubson <d.dubson@gmail.com>
2016-09-21 14:40:17 -0400

    Added mars exploration solution

--- src/main/java/com/ddubson/datastructures/trees/BalancedBST.java
+++ src/main/java/com/ddubson/datastructures/trees/BalancedBST.java
@@ -7,7 +7,7 @@ public class BalancedBST {
 	public static void main(String... args) {
 		int[] sortedArray = { 1, 2, 4, 5, 6, 7, 8 };
 
-		TreeNode n = createBST(sortedArray, 0, sortedArray.length - 1);
+		TreeNode<Integer> n = createBST(sortedArray, 0, sortedArray.length - 1);
 		System.out.println(n.toString());
 	}
 
@@ -17,7 +17,7 @@ public class BalancedBST {
 		}
 
 		int mid = (start + end) / 2;
-		TreeNode root = new TreeNode(array[mid]);
+		TreeNode<Integer> root = new TreeNode<>(array[mid]);
 		root.setLeft(createBST(array, start, mid - 1));
 		root.setRight(createBST(array, mid + 1, end));
 
---
+++ src/main/java/com/ddubson/util/Common.java
@@ -0,0 +1,13 @@
+package com.ddubson.util;
+
+import java.util.Scanner;
+
+/**
+ * Created by ddubson on 9/19/2016.
+ */
+public class Common {
+    public static void readFromCmd() {
+        Scanner s = new Scanner(System.in);
+        s.nextInt();
+    }
+}
