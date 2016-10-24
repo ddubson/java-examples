13f07169dade1fdf5e14da5c31bec6ee3c375632
Dmitriy Dubson <d.dubson@gmail.com>
2016-10-24 12:51:41 -0400

    Fixed fibonacci sequences; created memoized iterative DP solution

--- src/main/java/com/ddubson/algorithms/FibonacciSequence.java
+++ src/main/java/com/ddubson/algorithms/FibonacciSequence.java
@@ -16,15 +16,32 @@ public class FibonacciSequence {
         System.out.println(ifib(5));
         System.out.println(ifib(10));
 
-        // Recursive with Memoization
+        // Recursive with Memoization (Memoized DP Algorithm)
         System.out.println(mfib(5));
         System.out.println(mfib(10));
+
+        // Iterative with Memoization (Bottom Up DP Algorithm)
+        System.out.println(imfib(5));
+        System.out.println(imfib(10));
     }
 
     private static Map<Integer, Long> memo = new HashMap<>();
 
+    private static long imfib(int i) {
+        long[] fib = new long[i+1];
+        for (int j = 1; j < fib.length; j++) {
+            if (j <= 2)
+                fib[j] = 1;
+            else {
+                fib[j] = fib[j-1]+fib[j-2];
+            }
+        }
+        return fib[i];
+    }
+
+
     private static long mfib(int i) {
-        if(i < 2) {
+        if(i <= 2) {
             return 1;
         }
 
@@ -38,13 +55,13 @@ public class FibonacciSequence {
     }
 
     public static long fib(long i) {
-        return i < 2 ? 1 : fib(i-1)+fib(i-2);
+        return i <= 2 ? 1 : fib(i-1)+fib(i-2);
     }
 
     public static long ifib(long fibNumber) {
-        int firstFib = 0;
+        int firstFib = 1;
         int secondFib = 1;
-        int counter = 1;
+        int counter = 3;
         int sum = 0;
         while(counter <= fibNumber) {
             sum = firstFib + secondFib;
