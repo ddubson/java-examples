42231168194ca9ad7b592dfa5d8c2eb87f24a740
Dmitriy Dubson <d.dubson@gmail.com>
2016-11-21 19:23:11 -0500

    Added Spring Session - Redis sample

---
+++ spring-session/redis-session/README.md
@@ -0,0 +1,3 @@
+Spring Session using Redis as backend
+
+Make sure you have Redis running on port 6379 and Spring Boot Session + Redis takes care of internals.
---
+++ spring-session/redis-session/src/main/java/session/Application.java
@@ -0,0 +1,37 @@
+package session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Author: ddubson
+ */
+@SpringBootApplication
+@EnableRedisHttpSession
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String welcome(HttpSession session) {
+        UUID sessionId = null;
+        Optional<UUID> sessionIdOpt = Optional.ofNullable((UUID)session.getAttribute("uid"));
+        if(!sessionIdOpt.isPresent()) {
+            sessionId = UUID.randomUUID();
+            session.setAttribute("uid", sessionId);
+        } else {
+            sessionId = sessionIdOpt.get();
+        }
+
+        return sessionId.toString();
+    }
+}
