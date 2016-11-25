dc3cf4245b5f8105a06ce9ac620eee713153edad
Dmitriy Dubson <d.dubson@gmail.com>
2016-11-25 14:18:09 -0500

    Added Round Robin scheduler example

--- algorithms_data_structures/src/main/java/concepts/algorithms/RoundRobinProcessScheduler.java
+++ algorithms_data_structures/src/main/java/concepts/algorithms/RoundRobinProcessScheduler.java
@@ -1,9 +1,11 @@
 package concepts.algorithms;
 
+import concepts.structures.queue.CircularQueue;
 import concepts.structures.queue.LinkedListQueue;
-import concepts.structures.queue.Queue;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 /**
@@ -15,18 +17,17 @@ public class RoundRobinProcessScheduler {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         int capacity = 10;
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        Queue<Process> processQueue = new LinkedListQueue<>(capacity);
-        Stream.iterate(0, n -> n + 1).limit(10).forEach((i) -> processQueue.enqueue(new Process(i)));
+        CircularQueue<Process> processQueue = new LinkedListQueue<>(capacity);
 
-        while (true) {
-            Process process = processQueue.first();
-            Future<Process> future = executorService.submit(process);
-            future.get();
-            //processQueue.rotate();
-        }
+        Stream.iterate(0, n -> n + 1).limit(10).forEach((i) -> processQueue.enqueue(new Process(i)));
+        Stream.iterate(0, n -> n + 1).forEach(i -> {
+            executorService.submit(processQueue.first());
+            processQueue.rotate();
+        });
+
     }
 
-    static class Process implements Callable<Process> {
+    static class Process implements Runnable {
         int id;
 
         public Process(int id) {
@@ -34,14 +35,13 @@ public class RoundRobinProcessScheduler {
         }
 
         @Override
-        public Process call() {
-            System.out.println(String.format("Running process %d", id));
+        public void run() {
+            System.out.println(String.format("Running process %d | Thread %s", id, Thread.currentThread().getName()));
             try {
                 Thread.sleep(delay);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return this;
         }
     }
 }
