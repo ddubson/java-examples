dee4c3809e3fed7060610a582f5d9e9283121c47
Dmitriy Dubson <d.dubson@gmail.com>
2016-12-04 11:39:56 -0500

    Added javafx views to javafx-simple project

--- java-gui/javafx-simple/src/main/java/app/Main.java
+++ java-gui/javafx-simple/src/main/java/app/Main.java
@@ -1,23 +1,69 @@
 package app;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
+		initRootLayout();
+		showPersonOverview();
+	}
+	
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Shows the person overview inside the root layout.
+	 */
+	public void showPersonOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(personOverview);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns the main stage.
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
 	}
 	
 	public static void main(String[] args) {
