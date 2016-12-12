ad98c0eed52991b07a501cc4a9984e1e131266ab
Dmitriy Dubson <d.dubson@gmail.com>
2016-12-12 13:28:52 -0500

    Added ArrayDeque implementation and Deque interface

--- algorithms-data-structures/src/main/java/concepts/structures/queue/CircularQueue.java
+++ algorithms-data-structures/src/main/java/concepts/structures/queue/CircularQueue.java
@@ -2,6 +2,10 @@ package concepts.structures.queue;
 
 /**
  * Author: ddubson
+ *
+ * Useful for:
+ *      - multi-player, turn-based games,
+ *      - round-robin scheduling
  */
 public interface CircularQueue<E> extends Queue<E> {
     void rotate();
