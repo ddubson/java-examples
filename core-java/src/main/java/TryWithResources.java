4fb0257f04285b4db75229da1cd393e34473d6cd
Dmitriy Dubson <d.dubson@gmail.com>
2021-01-03 18:05:21 -0500

    Try with resources example

--- core-java/src/main/java/TryWithResources.java
+++ core-java/src/main/java/TryWithResources.java
@@ -3,9 +3,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class TryWithResources {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(TryWithResources.class.getClass().getResourceAsStream("/input.txt"));
+        while(scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+
         Path path = Paths.get("src", "main", "resources");
         try (BufferedReader br = Files.newBufferedReader(path.resolve("input.txt"))) {
             while (br.ready()) {
